a235a4f89b14ba92ed290f82a91111d1627d25f5
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-12-01 00:46:48 +0000

    initial "real" commit; add LICENSE and main.on-add.relative-uda.rs

---
+++ README.md
@@ -0,0 +1,39 @@
+# Andrew's TaskWarrior Hooks
+
+**Installation:** Bulid the hooks with `cargo build --release`. Whatever hooks
+you want, find them in `target/release/` and link them into `~/.task/hooks`,
+changing the name as approriate.
+
+For example,
+
+```
+ln -s "$PWD/target/release/on-add-relative-uda" "$HOME/.task/hooks/on-add.relative-uda"
+```
+
+Or copy them. Whatever. Importantly **you have to change the name of the file.** I
+cannot produce binaries with `.`s in them because Cargo does not support this.
+
+## on-add.relative-uda
+
+Lets you set the `wait` and `until` fields of recurring tasks relative to your `due`
+date. `until` is taken to be a duration **after** the due date and `wait` is taken
+to be a date **until** the due date.
+
+**Installation.** Add two UDAs, `relwait` and `reluntil`, to your `~/.taskrc`.
+
+```
+uda.untilrel.type=duration
+uda.untilrel.label=Until (relative)
+uda.waitrel.type=duration
+uda.waitrel.label=Until (relative)
+```
+
+Then install this hook, and for any recurring tasks you can set a relative wait time.
+For example,
+
+```
+task add project:Home.HVAC due:2026-01-15 recur:quarterly relwait:4w 'Replace furnace filter'
+```
+
+sets a "Replace furnace filter" task to occur quarterly on the 15th of the first month of
+the quarter, with the task hidden until 4 weeks ahead of time.
--- src/main.on-add.relative-uda.rs
+++ src/main.on-add.relative-uda.rs
@@ -1,3 +1,98 @@
-fn main() {
-    println!("Hello, world!");
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use std::{io, process};
+use std::io::BufRead as _;
+
+use anyhow::Context as _;
+use tasklib::prelude::UdaValue;
+use tasklib::chrono;
+
+const HOOK_NAME: &str = "on-add.relative-uda";
+
+fn process_task_line(s: &str) -> Result<tasklib::Task, anyhow::Error> {
+    let mut task = s.parse::<tasklib::Task>()
+        .with_context(|| format!("parsing JSON \"{s}\""))?;
+
+    // Skip the recurring parent: status=recurring and has "recur" set
+    if *task.status() == tasklib::Status::Recurring && task.recur().is_some() {
+        return Ok(task);
+    }
+
+    // Attempt to parse UDAs.
+    let udas = task.udas();
+    let untilrel = udas.get("untilrel")
+        .map(UdaValue::as_uda_duration)
+        .transpose()
+        .map_err(anyhow::Error::from_boxed)
+        .with_context(|| format!("parsing 'untilrel' {:?}", udas.get("untilrel")))?;
+    let waitrel = udas.get("waitrel")
+        .map(UdaValue::as_uda_duration)
+        .transpose()
+        .map_err(anyhow::Error::from_boxed)
+        .with_context(|| format!("parsing 'waitrel' {:?}", udas.get("waitrel")))?;
+    
+    // Skip tasks without due dates
+    let due = match task.due() {
+        Some(due) => *due,
+        None => {
+            if untilrel.is_some() {
+                eprintln!("Warning [task {}]: task has 'untilrel' set but no 'due' date. Ignoring 'untilrel'.", task.uuid());
+            }
+            if waitrel.is_some() {
+                eprintln!("Warning [task {}]: task has 'waitrel' set but no 'due' date. Ignoring 'waitrel'.", task.uuid());
+            }
+            return Ok(task);
+        }
+    };
+
+    // Annoyingly all UdaValue::as_uda_duration does is parse a string
+    // into a duration, leaving the type as a UdaValue even though it's
+    // guaranteed that it will be a datetime.
+
+    // Compute until = due + untilrel
+    if let Some(UdaValue::Duration(offset)) = untilrel {
+        let offset = chrono::Duration::seconds(i64::from(offset.num_seconds()));
+        if let Some(until) = task.until() {
+            eprintln!("Warning [task {}]: clobbering existing 'until' value {until} with {}", task.uuid(), due + offset);
+        }
+        *task.until_mut() = Some(due + offset);
+    }
+
+    // Compute wait = due - waitrel
+    if let Some(UdaValue::Duration(offset)) = waitrel {
+        let offset = chrono::Duration::seconds(i64::from(offset.num_seconds()));
+        if let Some(wait) = task.wait() {
+            eprintln!("Warning [task {}]: clobbering existing 'wait' value {wait} with {}", task.uuid(), due - offset);
+        }
+        *task.wait_mut() = Some(due - offset);
+    }
+
+    Ok(task)
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    let stdin = io::stdin();
+    let handle = stdin.lock();
+
+    for line_result in handle.lines() {
+        let line = match line_result {
+            Ok(l) => l,
+            Err(e) => {
+                eprintln!("relative-until hook: read error: {e}");
+                process::exit(1);
+            }
+        };
+
+        match process_task_line(&line) {
+            Ok(task) => task.to_stdout()?,
+            Err(e) => {
+                eprintln!("Hook {HOOK_NAME} failed. Passing unmodified task through.");
+                eprintln!("Error:");
+                eprintln!("{e}");
+                println!("{line}");
+            }
+        }
+    }
+
+    Ok(())
 }
